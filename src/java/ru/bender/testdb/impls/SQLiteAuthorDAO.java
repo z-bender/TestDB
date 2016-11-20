d83831bf4bbf01dfd4f92197068a20c3e2bc2033
bender <kazbender@mail.ru>
2016-11-20 16:21:04 +0300

    insert author

--- src/java/ru/bender/testdb/impls/SQLiteAuthorDAO.java
+++ src/java/ru/bender/testdb/impls/SQLiteAuthorDAO.java
@@ -1,7 +1,9 @@
 package ru.bender.testdb.impls;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Component;
 import ru.bender.testdb.interfaces.Author;
 import ru.bender.testdb.interfaces.AuthorDAO;
@@ -12,19 +14,35 @@ import javax.sql.DataSource;
 public class SQLiteAuthorDAO implements AuthorDAO {
 
     NamedParameterJdbcTemplate jdbcTemplate;
+    SimpleJdbcInsert authorInsert;
 
     @Autowired
     public void setDataSource(DataSource dataSource) {
         jdbcTemplate = new NamedParameterJdbcTemplate(dataSource);
+        authorInsert = new SimpleJdbcInsert(dataSource)
+                .withTableName("author")
+                .usingColumns("name")
+                .usingGeneratedKeyColumns("id");
     }
 
     @Override
     public int insert(Author author) {
-        return 0;
+        int id = authorInsert.executeAndReturnKeyHolder(getMapSqlParameterSource(author))
+                .getKey()
+                .intValue();
+        author.setID(id);
+        return id;
     }
 
     @Override
     public int getAuthorIdByName(String name) {
         return 0;
+    }
+
+    private MapSqlParameterSource getMapSqlParameterSource(Author author) {
+        MapSqlParameterSource params = new MapSqlParameterSource();
+        params.addValue("id", author.getID());
+        params.addValue("name", author.getName());
+        return params;
     }
 }
